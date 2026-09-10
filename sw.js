21d5ac006a675f1559e386ffb84f6415b3b790f3
Luisla425 <luislarez62@gmail.com>
2026-09-10 08:23:04 -0400

    fix: notificaciones via Service Worker

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('lm-v3').then(c=>c.addAll(['/','index.html','sw.js'])));
+  e.waitUntil(caches.open('lm-v4').then(c=>c.addAll(['/','index.html','sw.js'])));
 });
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('notificationclick',e=>{
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
+});
+self.addEventListener('message',e=>{
+  if(e.data&&e.data.type==='SHOW_NOTIF'){
+    self.registration.showNotification(e.data.title,{
+      body:e.data.body,
+      icon:'icon-192.png',
+      badge:'icon-192.png',
+      tag:e.data.tag
+    });
+  }
+});
